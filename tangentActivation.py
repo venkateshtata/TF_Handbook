7783ce37f50f5ccaef36d318b2eff7659700c46c
venkateshtata <venkateshtata9@gmail.com>
2020-05-21 12:48:59 +0530

    hyperbolic tangent activation function

---
+++ tangentActivation.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+h = np.linspace(-10, 10, 50)
+
+out = tf.tanh(h)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+	sess.run(init)
+	y = sess.run(out)
+	plt.xlabel('input value')
+	plt.ylabel('output value')
+	plt.title('Hyperbolic Tangent Activation Function')
+	plt.plot(h, y)
+	plt.show()
