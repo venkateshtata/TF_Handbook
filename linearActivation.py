75a17d1565b35fd04c24ec559d488106084df25a
venkateshtata <venkateshtata9@gmail.com>
2020-05-21 17:56:09 +0530

    Linear Activation Function

---
+++ linearActivation.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+X_in = tf.Variable(tf.random_normal([1,3], 0, 1))
+
+b = tf.Variable(tf.random_normal([1,1], 0, 1))
+w = tf.Variable(tf.random_normal([3,1], 0, 1))
+
+output = tf.matmul(X_in, w) + b
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+	sess.run(init)
+	print("X_in", sess.run(X_in))
+	print("w", sess.run(w))
+	out = sess.run(output)
+print(out)
